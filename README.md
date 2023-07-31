a19d90fa3853ae80dc9c989213052b859905ead7
crousekimo <43980978+crousekimo@users.noreply.github.com>
2023-07-31 19:55:55 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# MicrobittoJQ8900
+
+Micro:Bit控制JQ8900積木
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+```package
+pxt-jq8900=github:crousekimo/pxt-jq8900
+```
---
+++ main.ts
@@ -0,0 +1,80 @@
+//% weight=0 color=#331199 icon="\uf0ad" block="microbittojq8900"
+namespace microbittojq8900 {
+let readserialdata='';
+
+    //% blockId=setMicrobit block="Initialize Microbit |TX %tx|RX %rx|Baud rate %baudrate "
+    //% tx.defl=SerialPin.P0
+    //% rx.defl=SerialPin.P1
+    //% weight=102
+    //% blockExternalInputs = 1
+    export function setMicrobit(tx: SerialPin, rx: SerialPin, baudrate: BaudRate) {
+        serial.redirect(
+            tx,
+            rx,
+            baudrate
+        )
+        basic.pause(100)
+    }
+
+    //% blockId=playmp3 block="JQ8900 play mp3 %id "
+    //% weight=100
+    export function playmp3(id: number) {
+            let _a=pins.createBuffer(4)
+    	    _a[0] = 170
+            _a[1] = parseInt(convertToText(id),16)
+            _a[2] = 0
+            _a[3] = 172
+    	    serial.writeBuffer(_a)
+    }    
+    //% blockId=pausemp3 block="JQ8900 pause mp3"
+    //% weight=100
+    export function pausemp3() {
+            let _a=pins.createBuffer(4)
+    	    _a[0] = 170
+            _a[1] = 3
+            _a[2] = 0
+            _a[3] = 173
+    	    serial.writeBuffer(_a)
+    }    
+    //% blockId=stopmp3 block="JQ8900 stop mp3"
+    //% weight=100
+    export function stopmp3() {
+            let _a=pins.createBuffer(4)
+    	    _a[0] = 170
+            _a[1] = 4
+            _a[2] = 0
+            _a[3] = 174
+    	    serial.writeBuffer(_a)
+    }
+    //% blockId=previousmp3 block="JQ8900 play previous mp3"
+    //% weight=100
+    export function previousmp3() {
+            let _a=pins.createBuffer(4)
+    	    _a[0] = 170
+            _a[1] = 5
+            _a[2] = 0
+            _a[3] = 175
+    	    serial.writeBuffer(_a)
+    }
+    //% blockId=nextmp3 block="JQ8900 play next mp3"
+    //% weight=100
+    export function nextmp3() {
+            let _a=pins.createBuffer(4)
+    	    _a[0] = 170
+            _a[1] = 6
+            _a[2] = 0
+            _a[3] = 176
+    	    serial.writeBuffer(_a)
+    }  
+    //% blockId=setmp3vol block="JQ8900 set mp3 (0-20) %id volume"
+    //% weight=100
+    export function setmp3vol(vol:number) {
+            let _a=pins.createBuffer(5)
+    	    _a[0] = 170
+            _a[1] = 13
+            _a[2] = 1
+            _a[3] = parseInt(convertToText(vol),16)
+            _a[4] = parseInt("BE",16)+vol
+    	    serial.writeBuffer(_a)
+    }
+}
